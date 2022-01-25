1c1fb2faa86c94ad05b6f62a27ce29bf54ee6b40
rinorselmani9 <rs44583@ubt-uni.net>
2022-01-25 09:30:11 +0100

    useContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import UserResult from '../components/users/UserResult';
 
 function Home() {
   return <div>
-      <h1 className='text-6xl'>Welcome</h1>
+      <UserResult></UserResult>
   </div>;
 }
 
